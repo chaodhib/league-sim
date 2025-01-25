6b265d1813f06c635f7541980d187498b053aa0f
Chaouki Dhib <chaodhib@gmail.com>
2025-01-25 11:24:43 +0100

    update game data files

--- league-sim-backend/update_data.sh
+++ league-sim-backend/update_data.sh
@@ -14,8 +14,8 @@ cat source_3/champions.json | json_pp > source_3/champions_formatted.json
 wget -O source_4/121.json https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champions/121.json
 cat source_4/121.json | json_pp > source_4/121_formatted.json
 
-wget -O data/champions/khazix.bin.json https://raw.communitydragon.org/pbe/game/data/characters/khazix/khazix.bin.json
-cat data/champions/khazix.bin.json | json_pp > data/champions/khazix.bin_formated.json
+# wget -O data/champions/khazix.bin.json https://raw.communitydragon.org/pbe/game/data/characters/khazix/khazix.bin.json
+# cat data/champions/khazix.bin.json | json_pp > data/champions/khazix.bin_formated.json
 
 wget -O source_1/perks.json https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/perks.json
 cat source_1/perks.json | json_pp > source_1/perks_formated.json
