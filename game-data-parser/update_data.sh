33e4e565a61a994da31e9cd678dae8139747d7b7
Chaouki Dhib <chaodhib@gmail.com>
2025-02-22 23:29:04 +0100

    update game data

--- game-data-parser/update_data.sh
+++ game-data-parser/update_data.sh
@@ -1,23 +1,15 @@
  #!/bin/bash
 
-wget -O source_3/champions/Khazix.json http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions/Khazix.json
-
-# wget -O source_1/items.json https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/items.json
-# cat source_1/items.json | json_pp > source_1/items_formatted.json
-
 wget -O source_2/items.cdtb.bin.json https://raw.communitydragon.org/latest/game/items.cdtb.bin.json
 cat source_2/items.cdtb.bin.json | json_pp > source_2/items_formatted.json
 
-wget -O source_3/items.json http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/items.json
-wget -O source_3/champions.json http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions.json
-cat source_3/items.json | json_pp > source_3/items_formatted.json
-cat source_3/champions.json | json_pp > source_3/champions_formatted.json
+# wget -O source_3/items.json http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/items.json
+# wget -O source_3/champions.json http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions.json
+# cat source_3/items.json | json_pp > source_3/items_formatted.json
+# cat source_3/champions.json | json_pp > source_3/champions_formatted.json
 
 # wget -O source_3/champions/Khazix.json http://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/champions/Khazix.json
-# cat source_3/champions/Khazix.json | json_pp > source_3/champions/Khazix_formatted.json
 
-# wget -O source_4/121.json https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champions/121.json
-# cat source_4/121.json | json_pp > source_4/121_formatted.json
-
-# wget -O source_1/perks.json https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/perks.json
-# cat source_1/perks.json | json_pp > source_1/perks_formated.json
+cp $MERAKI_LOL_STATIC_DATA_PATH/items.json source_3/items.json
+cp $MERAKI_LOL_STATIC_DATA_PATH/champions.json source_3/champions.json
+cp $MERAKI_LOL_STATIC_DATA_PATH/champions/Khazix.json source_3/champions/Khazix.json
